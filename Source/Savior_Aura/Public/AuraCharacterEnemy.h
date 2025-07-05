e4b91d5a68deba78e349bfc9c40aa2753a6a2e5a
ling-shixi <109965842+ling-shixi@users.noreply.github.com>
2025-07-05 15:29:31 +0800

    提交美术素材

---
+++ Source/Savior_Aura/Public/AuraCharacterEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Charactor/AuraCharacterBase.h"
+#include "AuraCharacterEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAVIOR_AURA_API AAuraCharacterEnemy : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
